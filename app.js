88a01b51d27edc65b3e5fe16979a3b1e6b9b7b0b
adityaemaulana <adityaemaulana@gmail.com>
2019-07-01 16:44:59 +0700

    merge frontend and backend

--- app.js
+++
@@ -1,8 +0,0 @@
-import AppConfig from './app.routes.js';
-import LandingController from './modules/landing_page/landing_page.controller.js';
-
-var app = angular.module('app', ["ngRoute"]);
-
-app.config(AppConfig);
-
-app.controller('landingController', LandingController);
--- data/landing_page.data.js
+++
@@ -1,5 +0,0 @@
-export default [
-    "https://hookagency.com/wp-content/uploads/2015/11/light-blue-gradient-ui-gradient.jpg",
-    "https://webgradients.com/public/webgradients_png/064%20Red%20Salvation.png",
-    "https://cdn.shopify.com/s/files/1/2658/1334/products/HJ08064_1024x1024.jpg?v=1555579410"
-];
